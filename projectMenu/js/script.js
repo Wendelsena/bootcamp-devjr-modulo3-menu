83e6d5c953fe49c394fb8b068185037e9641ac23
Wendelsena <wendel4019@gmail.com>
2024-03-20 20:44:15 -0300

    closes #6 closes #7 - Add style.css page and imgs

--- projectMenu/js/script.js
+++ projectMenu/js/script.js
@@ -1,26 +1,29 @@
 var prods = [
-    { id: 1, name: "Produto 1", price: 11.0 },
-    { id: 2, name: "Produto 2", price: 22.0 },
-    { id: 3, name: "Produto 3", price: 33.0 },
-    { id: 4, name: "Produto 4", price: 40.0 },
-    { id: 5, name: "Produto 5", price: 22.0 },
-    { id: 6, name: "Produto 6", price: 24.0 },
-    { id: 7, name: "Produto 7", price: 25.0 },
-    { id: 8, name: "Produto 8", price: 27.0 },
-    { id: 9, name: "Produto 9", price: 29.0 }
+    { id: 1, name: "Cheeseburger", price: 11.0 },
+    { id: 2, name: "Bacon Sandwich", price: 22.0 },
+    { id: 3, name: "Cheese Fries", price: 33.0 },
+    { id: 4, name: "Chicken Quesadilla", price: 40.0 },
+    { id: 5, name: "Classic Burger", price: 22.0 },
+    { id: 6, name: "French Fries", price: 24.0 },
+    { id: 7, name: "Fried Shrimp", price: 25.0 },
+    { id: 8, name: "Cheese Sandwich", price: 27.0 },
+    { id: 9, name: "Salad Sandwich", price: 29.0 }
 ];
 
 function calc() {
     var quantities = document.getElementsByName("quantity");
     var output = document.getElementById("output");
     var total = 0;
-    var formatter = new Intl.NumberFormat('pt-BR', {
+    var userName = document.getElementById("name").value;
+    var formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: 'BRL',
+        currency: 'USD',
     });
 
     output.innerHTML = "";
 
+    output.innerHTML = `Dear <strong>${userName}</strong>, <br><br>Here are the details of your order:<br><br>`;
+    
     for (var input of quantities) {
         var id = parseInt(input.id);
         var quantity = parseInt(input.value);
@@ -30,9 +33,9 @@ function calc() {
             var totalPrice = prods[index].price * quantity;
             total += totalPrice;
 
-            output.innerHTML += `Produto: ${prods[index].name} - Preço unitário: ${formatter.format(prods[index].price)} - Quantidade: ${quantity} - <strong>Total relativo: ${formatter.format(totalPrice)}</br>`;
+            output.innerHTML += `• ${prods[index].name} - Unit price: ${formatter.format(prods[index].price)} - Quantity: ${quantity} - <strong>Subtotal: ${formatter.format(totalPrice)}</br>`;
         }
     }
 
-    output.innerHTML += `<h2>Total Geral: ${formatter.format(total)}</h2>`;
+    output.innerHTML += `<h2>Total: ${formatter.format(total)}</h2>`;
 }
